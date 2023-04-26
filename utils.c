a750d673a30d3fcbb77f0250b58f04809c61d643
shamzaou <shamzaou@student.42abudhabi.ae>
2023-04-26 04:31:03 +0400

    C - printf

--- utils.c
+++ utils.c
@@ -20,6 +20,12 @@ int	_strlen(char *s)
 	return (len);
 }
 
+/**
+ * _putchar - writes a character to stdout
+ * @c: character to be written
+ *
+ * Return: 1 on success, -1 on error
+ */
 int	_putchar(char c)
 {
 	return (write(1, &c, 1));
